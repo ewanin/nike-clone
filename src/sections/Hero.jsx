9ff04c882ea9739f51334b892c3ee91e1d8e01ae
Sahil Ninawe <ninawesahil12@gmail.com>
2024-01-17 21:15:00 +0530

    V1

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,53 @@
+import Button from "../components/Button"
+import {arrowRight} from '../assets/icons'
+import { shoes, statistics } from "../constants"
+import { bigShoe1, bigShoe2, bigShoe3 } from "../assets/images"
+import ShoeCard from "../components/ShoeCard"
+import { useState } from "react"
+
+const Hero = () => {
+
+const [bigShoeImage, setBigShoeImage] = useState(bigShoe1);
+
+  return (
+    <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
+      <div className="relative xl:w-2/5 flex flex-col justif-center items-start w-full max-xl:padding-x pt-28">
+        <p className="text-xl font-montserrat text-coral-red">Our Summer Collection</p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
+          <br />
+          <span className="text-coral-red inline-block mt-3">Nike</span>
+          <span > Shoes</span>
+        </h1>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
+        <Button label="Shop now" iconURL={arrowRight} />
+
+        <div className="flex justify-starts items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stat) => (
+            <div key={stat.label}>
+              <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">{stat.label}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        <img src={bigShoeImage} alt="shoe collection" width={610} height={502} className="object-contain relative z-10" />
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] max-sm:px-6">
+          {shoes.map((shoe) => (
+            <div key={shoe.thumbnail}>
+              <ShoeCard
+              imgURL={shoe}
+              changeBigShoeImage= {(shoe) => setBigShoeImage (shoe)}
+              bigShoeImage={bigShoeImage}
+               />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/sections/Services.jsx
@@ -0,0 +1,17 @@
+import ServiceCard from "../components/ServiceCard"
+import { services } from "../constants"
+
+const Services = () => {
+  return (
+    <div className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label}
+        {...service}
+
+        />
+      ))}
+    </div>
+  )
+}
+
+export default Services
